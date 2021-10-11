b1af4d555cfe52184b318c30e9c854b911b9c930
Itzsten <87199260+Itzsten@users.noreply.github.com>
2021-10-11 09:21:32 +0200

    Update index.js

--- idiot/index.js
+++ idiot/index.js
@@ -8,6 +8,7 @@ function setWindow() {
 function openWindow() {
 	for (let i = 0; i < 100; ++i) {
   		window.open("index.html", "_blank", "resizable=no,width=200,height=100,top=" + randomRange(0, screen.width).toString() + ',left=' + randomRange(0, screen.height).toString());
+		  setTimeout(function(i) { $('#ifr').attr('src', array[i]); }, 200 * i,i);
 	}
 }
 function moveWindowLoop() {
